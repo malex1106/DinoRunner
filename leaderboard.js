1a2b6fa78e42252caa969ae9b399a76bf1cb2ace
malex1106 <malex1106@gmail.com>
2022-10-24 11:55:36 +0200

    Update

--- leaderboard.js
+++ leaderboard.js
@@ -20,53 +20,28 @@ if(window?.data) {
   var text = document.createElement('h3');
   text.innerHTML = `Congrats ${data.name}, you achieved a score of ${data.score}!`
   var element = document.getElementById('yourscore');
-  element.appendChild(text);
   var sep = document.createElement('hr');
+  element.appendChild(text);
   element.appendChild(sep);
 }
 
 // Load leaderboard
 const db = getFirestore(app);
-const q = query(collection(db, "records"), orderBy("score", "desc"), limit(20));
+const q = query(collection(db, "records"), orderBy("score", "desc"), limit(100));
 const querySnapshot = await getDocs(q);
 
-// Table
-var table = document.createElement('table');
-var tableBody = document.createElement('tbody');
-var row = document.createElement('tr');
-var th_place = document.createElement('th');
-th_place.appendChild(document.createTextNode("#"));
-row.appendChild(th_place);
-var th_name = document.createElement('th');
-th_name.appendChild(document.createTextNode("Username"));
-row.appendChild(th_name);
-var th_score = document.createElement('th');
-th_score.appendChild(document.createTextNode("Score"));
-row.appendChild(th_score);
-tableBody.appendChild(row);
-
+const snaps = []
 var i = 0;
 querySnapshot.forEach((doc) => {
   // Counter
   i += 1;
-
-  var row = document.createElement('tr');
-  // PLACE
-  var place = document.createElement('td');
-  place.appendChild(document.createTextNode(i));
-  row.appendChild(place);
-
-  // NAME
-  var name = document.createElement('td');
-  name.appendChild(document.createTextNode(doc.data()['name']));
-  row.appendChild(name);
-
-  // SCORE
-  var score = document.createElement('td');
-  score.appendChild(document.createTextNode(doc.data()['score']));
-  row.appendChild(score);
-  tableBody.appendChild(row);
+  snaps[i-1] = [i, doc.data()['name'], doc.data()['score']];
 });
-
-table.appendChild(tableBody);
-document.body.appendChild(table);
+let tab = new DataTable('#example', {
+    data: snaps,
+    columns: [
+            { title: '#' },
+            { title: 'Username' },
+            { title: 'Score' },
+        ],
+});
